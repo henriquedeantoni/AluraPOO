529f2e815543305b08f217837ebffb8936a8f642
henrideanton <henriqdeantoni@gmail.com>
2024-12-06 01:17:58 -0300

    feat: incluido a selecao e condicao

--- Program.cs
+++ Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string mensagemBoasVindas = "Boas Vindas ao Screen Sound";
+
+void ExibirMensagemBoasVindas()
+{
+    Console.WriteLine(@"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░");
+
+    Console.WriteLine("\n\t " + mensagemBoasVindas+"\n");
+
+}
+
+void ExibirOpcoesMenu()
+{
+    Console.WriteLine("\t Digite 1 para registrar banda.");
+    Console.WriteLine("\t Digite 2 para mostrar todas as bandas.");
+    Console.WriteLine("\t Digite 3 para avaliar uma banda.");
+    Console.WriteLine("\t Digite 4 para exibir a média de uma banda.");
+    Console.WriteLine("\t Digite -1 para sair. \n");
+
+    Console.Write("\t Digite a sua opção: ");
+
+    string opcaoEscolhida = Console.ReadLine()!;
+
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    switch(opcaoEscolhidaNumerica)
+    {
+        case 1:
+            Console.WriteLine("\t Voce escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 2:
+            Console.WriteLine("\t Voce escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 3:
+            Console.WriteLine("\t Voce escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 4:
+            Console.WriteLine("\t Voce escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case -1:
+            Console.WriteLine("\t Voce escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        default: Console.WriteLine("\t Opção Inválida. \n \t Encerrando a aplicação ");
+            break;
+    }
+}
+
+
+ExibirMensagemBoasVindas();
+ExibirOpcoesMenu();
