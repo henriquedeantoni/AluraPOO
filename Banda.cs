efadcb8c17641f1aacf9e1f47c06943e6e29a548
henrideanton <henriqdeantoni@gmail.com>
2024-12-17 23:55:04 -0300

    feat: criado construtor para a classe music

---
+++ Banda.cs
@@ -0,0 +1,20 @@
+﻿class Banda
+{
+    private List<Album> albums = new List<Album>();
+    public string Title { get; set; }
+
+    public void AddAlbum(Album album)
+    {
+        albums.Add(album);
+    }
+
+    public void ShowDiscography()
+    {
+        Console.WriteLine($"Discografia da banda {Title} é: \n ");
+        foreach(Album album in albums)
+        {
+            Console.WriteLine($"Álbum {album.Title} ({album.TotalDuration})");
+        }
+        
+    }
+}
