fda9ab9db5f74d1ab4892761706199159c035634
henrideanton <henriqdeantoni@gmail.com>
2024-12-18 23:55:06 -0300

    feat: criada classe podcast e classe episode

---
+++ Podcast.cs
@@ -0,0 +1,39 @@
+﻿class Podcast
+{
+    #region Constructor
+
+    public Podcast(string host, string title)
+    {
+        Host = host;
+        Title = title;
+    }
+
+    #endregion
+
+
+    #region Props
+
+    public string Host { get; }
+    public string Title { get;}
+    public int TotalEpisodes => EpisodeList.Count;
+    private List<Episode> EpisodeList = new List<Episode>();
+    #endregion
+
+    #region Methods
+    public void AddEpisodes(Episode episode)
+    {
+        EpisodeList.Add(episode);
+    }
+    public void ShowDetails()
+    {
+        Console.WriteLine($"Este é o Podcast {Title} e oferecido por {Host}");
+        Console.WriteLine($"\nPossui um total de {TotalEpisodes} Episódios.\nA lista de episódios pode ser vista aqui:\n");
+        foreach (Episode episode in EpisodeList.OrderBy(e=>e.Index))
+        {
+            Console.WriteLine(episode.Summary);
+        }
+    }
+    #endregion
+
+
+}
